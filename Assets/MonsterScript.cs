04939e21e77c9eb78a5b6c02c17987691d1fcce2
Eugeniov <bastien.coquelin@gmail.com>
2021-01-27 16:05:53 +0100

    ca va marcher

---
+++ Assets/MonsterScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScript : MonoBehaviour
+{
+    public float speed;
+
+    [SerializeField]
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        animator.SetBool("Start", true);
+    }
+}

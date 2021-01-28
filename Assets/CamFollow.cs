e83ce9205efcf60e06f5d7402e31acd17542ca2e
Eugeniov <bastien.coquelin@gmail.com>
2021-01-28 18:00:46 +0100

    rewrite script

--- Assets/CamFollow.cs
+++ Assets/CamFollow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CamFollow : MonoBehaviour
 {
+    private bool follow = true;
     public Transform player;
     public Vector3 offset;
     // Start is called before the first frame update
@@ -15,6 +16,11 @@ public class CamFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(follow == true)
         transform.position = new Vector3(player.position.x + offset.x, offset.y, offset.z);
+        if (player.position.x > 1000)
+        {
+            follow = false;
+        }
     }
 }
--- Assets/MovePlayer.cs
+++ Assets/MovePlayer.cs
@@ -32,7 +32,7 @@ public class MovePlayer : MonoBehaviour
 
         if(IsGrounded == true && Input.GetKeyDown(KeyCode.UpArrow))
         {
-            float jumpVelocity = 12.0f;
+            float jumpVelocity = 13.0f;
             rigidbody2d.velocity = Vector2.up * jumpVelocity;
             Debug.Log("Jump");
             animator.SetBool("IsGrounded", false);

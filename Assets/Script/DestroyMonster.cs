56ffb2c946c8d071ed8526e0246d88b8adf77f1e
Eugeniov <bastien.coquelin@gmail.com>
2021-01-29 17:18:35 +0100

    no laser

--- Assets/Script/DestroyMonster.cs
+++ Assets/Script/DestroyMonster.cs
@@ -20,7 +20,7 @@ public class DestroyMonster : MonoBehaviour
     {
         if(other.gameObject.tag == "Player")
         {
-            GameplayManager.Instance.Showlaser();
+            //GameplayManager.Instance.Showlaser();
             Destroy(monster);
         }
         
